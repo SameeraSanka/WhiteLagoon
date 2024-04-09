7490b49395327014adae950894491052ab3810ac
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-04-10 00:24:24 +0530

    03 - Villa Model 0.1

---
+++ WhiteLagoon.web/Controllers/VillaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+
+        public IActionResult Index()
+        {
+            var villa = _db.Villas.ToList();
+            return View(villa);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
